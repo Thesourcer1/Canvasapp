317b8c8e326d4efe54d4c3ad06a57c766209c483
sara <sara.henriksen95@hotmail.com>
2021-05-25 17:37:28 +0200

    all changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Canvas from './Components/Canvas';
+import CanvasSettings from './Components/CanvasSettings';
+import Export from './Components/Export';
+import Tools from './Components/Tools';
+import { Accordion, Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [colorPicker, setColorPicker] = useState('black');
+  const [canvasSettings, setCanvasSettings] = useState({ //object
+    width: 500, 
+    height: 500
+  });
+  const [brushSize, setBrushSize] = useState(5);
+
+  const [eraser, setEraserStatus] = useState(false);
+
+  const [canvasObject, setCanvasObject] = useState(null);
+  let canvasBox = document.getElementsByClassName("CanvasBox")[0]
+  console.log(canvasBox)
+
+  const [clearCanvas, setClearStatus] = useState(false);
+
+  const [fill, setFillStatus] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="CanvasBox">
+        <Canvas 
+        colorPicker = {colorPicker} 
+        canvasSettings = {canvasSettings} 
+        brushSize = {brushSize}
+        eraser = {eraser}
+        setCanvasObject = {setCanvasObject}
+        clearCanvas = {clearCanvas}
+        setClearStatus = {setClearStatus}
+        fill = {fill}
+        setFillStatus = {setFillStatus}
+        />
+      </div>
+      <div className="ToolBox">
+        <h1>Drawing app</h1>
+        {/*Canvas settings accordion*/}
+        <Accordion defaultActiveKey="0" style={{paddingTop: "20px"}}>
+            <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                  <h3>Canvas Settings</h3><i class="bi bi-chevron-down" style={{fontWeight:'bold', fontSize:'16px'}}></i>
+                  <hr></hr>
+                </Accordion.Toggle>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>
+                  <CanvasSettings setCanvasSettings = {setCanvasSettings}
+                  />
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="1">
+                  <h3>Tools</h3><i class="bi bi-chevron-down" style={{fontWeight:'bold', fontSize:'16px'}}></i>
+                  <hr></hr>
+                </Accordion.Toggle>
+              <Accordion.Collapse eventKey="1">
+                <Card.Body>
+                  <Tools 
+                  setColorPicker = {setColorPicker} 
+                  setBrushSize = {setBrushSize} 
+                  brushSize = {brushSize}
+                  setEraserStatus ={setEraserStatus}
+                  eraser = {eraser}
+                  setClearStatus = {setClearStatus}
+                  />
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+                <Accordion.Toggle as={Card.Header} eventKey="2">
+                  <h3>Export</h3><i class="bi bi-chevron-down" style={{fontWeight:'bold', fontSize:'16px'}}></i>
+                  <hr></hr>
+                </Accordion.Toggle>
+              <Accordion.Collapse eventKey="2">
+                <Card.Body>
+                  <Export
+                  canvasObject = {canvasObject}
+                  setFillStatus = {setFillStatus}
+                  />
+                </Card.Body>
+              </Accordion.Collapse>
+            </Card>
+        </Accordion>
+      </div>
     </div>
-  );
+    );
+  
 }
 
 export default App;
+
+
+
+  // const CanvasContainer = useState(canvasBox);
+
+  // const [scaleCanvas, setScaleCanvas] = useState(0.5);
+  
+  // const openAccordion = (e) => {
+  //   let chevrons = document.getElementsByClassName("bi-chevron-down")
+  //   for (let i = 0; i < chevrons.length; i++) {
+  //     chevrons[i].style.transform = "rotateX(0deg)";
+  //   }
+  //   let deg = 0;
+  //   e.target.childNodes[1].style.transform = `rotateX(${deg + 180}deg)`;
+  // }
